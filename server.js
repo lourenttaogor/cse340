4c81bc76e82211d517c430d4505d644d3ca0e51d
Laurelpere <pelau360@gmail.com>
2025-11-05 02:35:59 +0100

    week1

--- server.js
+++ server.js
@@ -6,9 +6,21 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+const static = require("./routes/static.js")
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
